38ddd06a029f2f6a592858a5571ccf4ee3759d9e
Raykel5Villar <raykelvillar@gmail.com>
2024-05-07 00:20:39 -0400

    add links, specify language, limit

--- main..py
+++ main..py
@@ -1,8 +1,9 @@
 import requests
 from send_email import send_email
 
+topic = "tesla"
 api_key = "0a16afc8fb7f49b6bb20eaa6135a1c1f"
-url = "https://newsapi.org/v2/everything?q=apple&from=2024-05-06&to=2024-05-06&sortBy=popularity&apiKey=0a16afc8fb7f49b6bb20eaa6135a1c1f"
+url = f'https://newsapi.org/v2/everything?q={topic}&from=2024-05-06&to=2024-05-06&sortBy=popularity&apiKey=0a16afc8fb7f49b6bb20eaa6135a1c1f&language=es'
 
 # Make Request
 request = requests.get(url)
@@ -10,10 +11,11 @@ request = requests.get(url)
 # Get a dictionary with data
 content = request.json()
 
-body = ""
-for article in content['articles']:
+body = "Subject: Today news\n"
+for article in content['articles'][:20]:
     if article['title'] is not None:
-        body = body + article['title'] + "\n" + article['description'] + 2*"\n"
+        body += article['title'] + "\n" + article['description'] + "\n" + article['url'] + 2*"\n"
     
 body = body.encode("utf-8")
 send_email(message=body)
+
